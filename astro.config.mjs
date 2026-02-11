dd859c6280dcf342355d9ee280d78de4b9ff75cc
Alfred <alfred.pi.assistant@gmail.com>
2026-02-11 16:38:32 +0100

    Restore test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 export default defineConfig({
-  site: 'https://jeunesseandco.ch', base: '/', compressHTML: true,
+  site: 'https://alfred-pi.github.io', base: '/jeunesse-co-site', compressHTML: true,
   image: { service: { config: { quality: 90 } } },
   build: { inlineStylesheets: 'auto' },
   vite: { build: { minify: 'esbuild', cssMinify: true, rollupOptions: { output: { manualChunks: { gsap: ['gsap', 'gsap/ScrollTrigger'] } } } } }
