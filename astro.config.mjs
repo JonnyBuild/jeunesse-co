5f817981387ba85bab5560f2c631e6c536e23a92
Alfred <alfred.pi.assistant@gmail.com>
2026-02-11 15:47:38 +0100

    Clean repo for public release - remove working files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://alfred-pi.github.io',
-  base: '/jeunesse-co-site',
+  site: 'https://jeunesseandco.ch',
+  base: '/',
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto'
