c640618237c808b1548abd055cc976cb09560747
Alfred <alfred.pi.assistant@gmail.com>
2026-02-11 17:43:57 +0100

    Trigger fresh deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 export default defineConfig({
-  site: 'https://alfred-pi.github.io', base: '/jeunesse-co-site', compressHTML: true,
+  site: 'https://jeunesseandco.ch',
+  base: '/',
+  compressHTML: true,
   image: { service: { config: { quality: 90 } } },
   build: { inlineStylesheets: 'auto' },
   vite: { build: { minify: 'esbuild', cssMinify: true, rollupOptions: { output: { manualChunks: { gsap: ['gsap', 'gsap/ScrollTrigger'] } } } } }
